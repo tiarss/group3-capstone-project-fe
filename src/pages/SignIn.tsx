a0f194eeae0849dd485b98ca41f3355ef469a04a
prime_ion <mohadipramudiono@gmail.com>
2022-02-15 20:08:51 +0700

    edited signin

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -3,25 +3,37 @@ import { Box, Button, Container, FormControl, FormErrorMessage, FormHelperText, 
 import { Link as ReactRouterLink } from "react-router-dom"
 import axios from "axios";
 import React, { useState } from "react";
+import { InputPassword, InputText } from "../components/Input";
 // import { InputText } from "../components/Input";
 
 const SignIn = () => {
   const [userLogin, setUserLogin] = useState<string>("");
   const [userPassword, setUserPassword] = useState<string>("");
+  const [userIsLoginError, setIsUserLoginError] = useState<boolean>(false);
+  const [userIsPasswordError, setIsUserPasswordError] = useState<boolean>(false);
   const [show, setShow] = React.useState(false);
   
-  const handlePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    setUserPassword(value)
-  };
   const handlePhoneEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
-    setUserLogin(value)
+    if (value===""){
+      setIsUserLoginError(true);
+    } else {
+      setIsUserLoginError(false);
+    }
+    setUserLogin(value);
   };
-  const handleClick = () => setShow(!show);
 
-  const isLoginError = userLogin === '';
-  const isPasswordError = userPassword === '';
+  const handlePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    if(value === ""){
+      setIsUserPasswordError(true);
+    } else {
+      setIsUserPasswordError(false);
+    }
+    setUserPassword(value);
+  };
+  
+  const handleClick = () => setShow(!show);
 
   const handleSignIn = () =>{
     axios.post('https://klender.xyz/login',{
@@ -35,24 +47,24 @@ const SignIn = () => {
   }
 
   return (
-      <Container maxW='container.md' marginTop='10' centerContent>
-        <Box width='100%' padding={5} border='1px' borderColor='gray.200' borderRadius='lg'>
-            {/* <InputText
-              label='Email/Phone'
-              type='text'
-              placeholder='Enter Name'
-              onChange={handlePhoneEmail}
-            /> */}
-            <FormControl isInvalid={isLoginError}>
-                <FormLabel htmlFor='email/phone' pt='5'>Email/Telepon</FormLabel>
-                <Input
-                    id='email/phone'
+    <>
+      {/* <Container maxW='container.md' marginTop='10' centerContent> */}
+        <Box
+        width='100%'
+        height='100vh'
+        display='flex'
+        alignItems='center'
+        justifyContent='center'
+        flexDir="column">
+        <Box width='75%' padding={5} border='1px' borderColor='gray.200' borderRadius='lg'>
+            <FormControl isInvalid={userIsLoginError}>
+                <InputText
+                    label='Email/Phone Number'
                     type='text'
-                    placeholder='Enter your Email!'
-                    value={userLogin}
+                    placeholder='Enter your email or phone number!'
                     onChange={handlePhoneEmail}
                 />
-                {!isLoginError ? (
+                {!userIsLoginError ? (
                     <FormHelperText mb='5'>
                     Enter your registered email or phone number!
                     </FormHelperText>
@@ -60,28 +72,13 @@ const SignIn = () => {
                     <FormErrorMessage mb='5'>Email or phone number is required.</FormErrorMessage>
                 )}
             </FormControl>
-            {/* <InputText
-              label='Password'
-              type='password'
-              placeholder='Enter Name'
-              onChange={handlePassword}
-            />
-            <button onClick={handleSignIn}>Login</button> */}
-            <FormControl isInvalid={isPasswordError}>
-                <FormLabel htmlFor='password'>Password</FormLabel>
-                <InputGroup size='md'>
-                    <Input
-                        id='password'
-                        pr='4.5rem'
-                        type={show ? 'text' : 'password'}
+            <FormControl isInvalid={userIsPasswordError}>
+                    <InputPassword
+                        label='Password'
                         placeholder='Enter password'
                         onChange={handlePassword}
                     />
-                    <InputRightElement width='4.5rem'>
-                        <IconButton size='sm' aria-label='Show password' onClick={handleClick} variant='ghost' icon={show ? <ViewOffIcon /> : <ViewIcon/>} />
-                    </InputRightElement>
-                </InputGroup>
-                {!isPasswordError ? (
+                {!userIsPasswordError ? (
                     <FormHelperText pb='5'>
                     Enter your password!
                     </FormHelperText>
@@ -92,7 +89,9 @@ const SignIn = () => {
             <Button colorScheme='blue' mb={5} width='100%' onClick={handleSignIn}>Sign In</Button>
             <Text fontSize='sm' align='center'>Don't have an account? <Link color='teal.500' as={ReactRouterLink} to='/sign-up'>Sign Up</Link></Text>
         </Box>
-      </Container>
+        </Box>
+      {/* </Container> */}
+    </>
   );
 };
 

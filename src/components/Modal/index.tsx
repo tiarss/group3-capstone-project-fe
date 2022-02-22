9e9c4f808c36f65da2c7aab400e591fb8271c1c0
prime_ion <mohadipramudiono@gmail.com>
2022-02-22 23:51:08 +0700

    modal tambah aset semi beres

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -12,20 +12,46 @@ import {
     FormLabel,
     Box,
     Select,
+    FormControl,
+    Switch,
   } from '@chakra-ui/react';
 import { InputText } from "../Input";
 
 const ModalAddAssets = () => {
     const { isOpen, onOpen, onClose } = useDisclosure();
-    const [kategoriAset, setKategoriAset] = useState<{id: number, deskripsi: string}[]>([{id:1, deskripsi: "Laptop"}, {id:2, deskripsi:"Alat"}]);
-    const [asetImage, setAsetImage] = useState<File>();
+    const [ namaAset, setNamaAset ] = useState<string>("");
+    const [ deskripsiAset, setDeskripsiAset ] = useState<string>("");
+    const [ kategoriAset, setKategoriAset ] = useState<{id: number, deskripsi: string}[]>([{id:1, deskripsi: "Laptop"}, {id:2, deskripsi:"Alat"}]);
+    const [ selectedCategoryId, setSelectedCategoryId ] = useState<number>(0);
+    const [ jumlahAset, setJumlahAset ] = useState<string | number>();
+    const [ asetImage, setAsetImage ] = useState<File>();
+    const [ isMaintained, setIsMaintained ] = useState<boolean>(false);
+
+    const handleNama = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        setNamaAset(value);
+    };
+
+    const handleDeskripsi = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        setDeskripsiAset(value);
+    };
+
+    const handleKategori = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = parseInt(e.target.value);
+        setSelectedCategoryId(value);
+    };
+
+    const handleJumlahAset = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = parseInt(e.target.value);
+        setJumlahAset(value);
+    };
 
     const handleAsetImage = (e: React.ChangeEvent<HTMLInputElement>) => {
         const value = e.target.files;
         if (!value) return;
     
         setAsetImage(value[0]);
-        //  console.log(value)
     };
 
     return (
@@ -38,24 +64,30 @@ const ModalAddAssets = () => {
               <ModalHeader>Tambah Asset</ModalHeader>
               <ModalCloseButton />
               <ModalBody>
-                <InputText title="Nama Aset" placeholder="Masukkan Nama Aset"/>
-                <InputText title="Deskripsi Aset" placeholder="Masukkan Deskripsi Aset"/>
-                {/* <InputText title="Kategori Aset"/> */}
+                <InputText title="Nama Aset" placeholder="Masukkan Nama Aset" onChange={handleNama}/>
+                <InputText title="Deskripsi Aset" placeholder="Masukkan Deskripsi Aset" onChange={handleDeskripsi}/>
                 <Box>
                     <FormLabel style={{ fontWeight: "bold" }}>Kategori Aset</FormLabel>
-                    <Select placeholder='Pilih Kategori Aset'>
+                    <Select placeholder='Pilih Kategori Aset' onChange={(e)=>handleKategori}>
                         {kategoriAset.map((item:any)=>(
-                        <option value={item.id}>{item.deskripsi}</option>
+                        <option key={item.id} value={item.id}>{item.deskripsi}</option>
                         ))}
                     </Select>
                 </Box>
-                <InputText title="Jumlah Aset" placeholder="Masukkan Jumlah Aset"/>
+                <InputText title="Jumlah Aset" placeholder="Masukkan Jumlah Aset" onChange={handleJumlahAset}/>
                 <Box>
                     <FormLabel style={{ fontWeight: "bold" }}>Foto Aset</FormLabel>
                     <input type='file' onChange={handleAsetImage} />
                 </Box>
+                <Box bgColor="#EFEFEF" marginTop={3} borderRadius={5}>
+                    <FormControl display='flex' alignItems='center'>
+                        <FormLabel htmlFor='email-alerts' mb='0'>
+                            Maintenance?
+                        </FormLabel>
+                        <Switch id='email-alerts'/>
+                    </FormControl>
+                </Box>
               </ModalBody>
-    
               <ModalFooter>
                 <Button colorScheme='blue' mr={3} onClick={onClose}>
                   Ajukan
